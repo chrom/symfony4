055c516de6c7b1bf15774c1e5a17ff78e2e4dc81
Andrey Zabara <dr.chrom@gmail.com>
2020-03-23 20:38:11 +0200

    18 Activating Timestampable && added updatedAt

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -4,12 +4,14 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ArticleRepository")
  */
 class Article
 {
+    use TimestampableEntity;
     /**
      * @ORM\Id()
      * @ORM\GeneratedValue()
@@ -34,11 +36,6 @@ class Article
     private $context;
 
     /**
-     * @ORM\Column(type="datetime")
-     */
-    private $createdAt;
-
-    /**
      * @ORM\Column(type="datetime", nullable=true)
      */
     private $publishedAt;
@@ -57,11 +54,6 @@ class Article
      * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $image;
-
-    /**
-     * @ORM\Column(type="datetime")
-     */
-    private $updatedAt;
 
     public function getId(): ?int
     {
@@ -172,18 +164,6 @@ class Article
     public function incrementHeartCount(): self
     {
         $this->heartCount += 1;
-        return $this;
-    }
-
-    public function getUpdatedAt(): ?\DateTimeInterface
-    {
-        return $this->updatedAt;
-    }
-
-    public function setUpdatedAt(\DateTimeInterface $updatedAt): self
-    {
-        $this->updatedAt = $updatedAt;
-
         return $this;
     }
 }
