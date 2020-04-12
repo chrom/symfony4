3a4704669d9ec99707c19572b9300f758c69e431
Andrew Zabara <dr.chrom@gmail.com>
2020-04-12 19:07:36 +0300

    Symfony Security: Beautiful Authentication, Powerful Authorization 33 Adding a Custom Voter

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -26,12 +26,11 @@ class ArticleAdminController extends AbstractController
 
     /**
      * @Route("/admin/article/{id}/edit")
+     * @IsGranted("MANAGE", subject="article")
      */
     public function edit(Article $article)
     {
-        if ($article->getAuthor() != $this->getUser() && !$this->isGranted('ROLE_ADMIN_ARTICLE')) {
-            throw $this->createAccessDeniedException('No access!');
-        }
+//        $this->denyAccessUnlessGranted('MANAGE', $article);
         dd($article);
     }
 }
