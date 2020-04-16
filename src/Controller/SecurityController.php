89832460ca2b6f876f4c795c6760b787f042d918
Andrew Zabara <dr.chrom@gmail.com>
2020-04-16 09:15:48 +0300

    Symfony 4 Forms: Build, Render & Conquer! 15 Adding Extra "Unmapped" Fields

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -58,7 +58,7 @@ class SecurityController extends AbstractController
 
             $user->setPassword($passwordEncoder->encodePassword(
                 $user,
-                $user->getPassword()
+                $form['plainPassword']->getData()
             ));
             $em = $this->getDoctrine()->getManager();
             $em->persist($user);
