5ca058fc41bbebfb666324ae3cd222aa3da5cbb4
Andrew Zabara <dr.chrom@gmail.com>
2020-06-30 21:41:37 +0300

    Symfony 4 Symfony 4 Forms: Build, Render & Conquer! 35 Setup: For Dependent Select Fields

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -9,6 +9,7 @@ use App\Entity\User;
 use App\Repository\UserRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -42,7 +43,21 @@ class ArticleFormType extends AbstractType
             ->add('title')
             ->add('author', UserSelectTextType::class,
                 ['disabled' => $isEdit]
-            );
+            )
+            ->add('location', ChoiceType::class, [
+                'choices' => [
+                    'The Solar System' => 'solar_system',
+                    'Near a star' => 'star',
+                    'Interstellar Space' => 'interstellar_space'
+                ],
+                'required' => false,])
+            ->add('specificLocationName', ChoiceType::class, [
+                'placeholder' => 'Where exactly?',
+                'choices' => [
+                    'TODO' => 'TODO'
+                ],
+                'required' => false,
+            ]);
         if ($options['include_published_at']) {
             $builder->add('publishedAt', null, [
                 'widget' => 'single_text'
