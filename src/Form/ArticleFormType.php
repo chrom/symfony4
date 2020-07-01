4248522ceec81110c1807549ab9b521f17abe3a5
Andrew Zabara <dr.chrom@gmail.com>
2020-07-01 21:28:33 +0300

    Symfony 4 Symfony 4 Forms: Build, Render & Conquer! 36 Form Events & Dynamic ChoiceType choices

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -5,14 +5,10 @@ namespace App\Form;
 
 
 use App\Entity\Article;
-use App\Entity\User;
 use App\Repository\UserRepository;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -36,6 +32,7 @@ class ArticleFormType extends AbstractType
         /** @var Article $article */
         $article = $options['data'] ?? null;
         $isEdit = $article && $article->getId();
+        $location = $article ? $article->getLocation() : null;
         $builder
             ->add('content', TextareaType::class, [
                 'help' => 'Choose something catchy!',
@@ -45,22 +42,23 @@ class ArticleFormType extends AbstractType
                 ['disabled' => $isEdit]
             )
             ->add('location', ChoiceType::class, [
-                'choices' => [
-                    'The Solar System' => 'solar_system',
-                    'Near a star' => 'star',
-                    'Interstellar Space' => 'interstellar_space'
-                ],
-                'required' => false,])
-            ->add('specificLocationName', ChoiceType::class, [
-                'placeholder' => 'Where exactly?',
-                'choices' => [
-                    'TODO' => 'TODO'
-                ],
-                'required' => false,
-            ]);
+                    'choices' => [
+                        'The Solar System' => 'solar_system',
+                        'Near a star' => 'star',
+                        'Interstellar Space' => 'interstellar_space'
+                    ],
+                    'required' => false]
+            );
         if ($options['include_published_at']) {
             $builder->add('publishedAt', null, [
                 'widget' => 'single_text'
+            ]);
+        }
+        if ($location) {
+            $builder->add('specificLocationName', ChoiceType::class, [
+                'placeholder' => 'Where exactly?',
+                'choices' => $this->getLocationNameChoices($location),
+                'required' => false,
             ]);
         }
     }
@@ -71,4 +69,32 @@ class ArticleFormType extends AbstractType
             'include_published_at' => false]);
     }
 
+    private function getLocationNameChoices(string $location)
+    {
+        $planets = [
+            'Mercury',
+            'Venus',
+            'Earth',
+            'Mars',
+            'Jupiter',
+            'Saturn',
+            'Uranus',
+            'Neptune',
+        ];
+        $stars = [
+            'Polaris',
+            'Sirius',
+            'Alpha Centauari A',
+            'Alpha Centauari B',
+            'Betelgeuse',
+            'Rigel',
+            'Other'
+        ];
+        $locationNameChoices = [
+            'solar_system' => array_combine($planets, $planets),
+            'star' => array_combine($stars, $stars),
+            'interstellar_space' => null,
+        ];
+        return $locationNameChoices[$location];
+    }
 }
