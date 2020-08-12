1ed8d8b94acb80124d87a08971b6f01c8d1a2216
Andrew Zabara <dr.chrom@gmail.com>
2020-08-12 14:40:45 +0300

    Symfony 4 Symfony 4 Forms: 41 A bit of Security Cleanup

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -1,21 +1,14 @@
 <?php
-
 declare(strict_types=1);
 
-
 namespace App\Controller;
-
 
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+/**
+ * @method User getUser()
+ */
 abstract class BaseController extends AbstractController
 {
-    /**
-     * @return User|null
-     */
-    public function getUser(): ?User
-    {
-        return parent::getUser();
-    }
 }
